958a3625b39067f16af68c994b9e543f053198d4
Cory Knapp <knapp.cory@gmail.com>
2019-08-06 15:52:36 -0700

    improved demo

--- demo/src/states/play.js
+++ demo/src/states/play.js
@@ -10,9 +10,9 @@ PlayState = new Kiwi.State('PlayState');
 PlayState.create = function (params) {
 
 	// set some constants
-	this.hexSize = 36;
-	this.horizontalHexCount = 32;
-	this.verticalHexCount = 32;
+	this.hexSize = 64;
+	this.horizontalHexCount = 16;
+	this.verticalHexCount = 16;
 
 	// create lines to draw hexes
 	this.hexPolygons = [];
@@ -40,7 +40,6 @@ PlayState.create = function (params) {
 				polyVerts.push( [hexCorner.i, hexCorner.j] )
 			}
 			var hexPolygon = new Kiwi.Plugins.Primitives.Polygon( {
-				color: [i/this.horizontalHexCount, j/this.verticalHexCount, 1.0],
 				state: this,
 				indices: [ 0, 1, 2, 0, 3, 4, 0, 5],
 				vertices: polyVerts
@@ -49,6 +48,10 @@ PlayState.create = function (params) {
 			this.addChild( hexPolygon );
 		}
 	}
+	this.recolorHexes()
+
+	this.game.input.mouse.onDown.add( this.recolorHexes, this );
+	
 }
 
 PlayState.update = function () {
@@ -62,5 +65,13 @@ PlayState.update = function () {
 	if( hexCoords.i >= 0 && hexCoords.i < this.horizontalHexCount
 		&& hexCoords.j >= 0 && hexCoords.j < this.verticalHexCount ){
 		this.hexPolygons[hexCoords.i][hexCoords.j].color = [1.0, 0.0, 0.0];
+	}
+}
+
+PlayState.recolorHexes = function() {
+	for (i = 0; i < this.horizontalHexCount; i++) {
+		for (j = 0; j < this.verticalHexCount; j++) {
+			this.hexPolygons[i][j].color = [i/this.horizontalHexCount, j/this.verticalHexCount, 1.0];
+		}
 	}
 }
